1e80afc10716af6635e76bd5ddadc5e41bf704d6
Preethi Chidambaram <pkc7dbu@virginia.edu>
2024-03-09 22:35:32 +0000

    props as types latest

--- Student/props_as_types.lean
+++ Student/props_as_types.lean
@@ -31,3 +31,40 @@ This demonstrates that True implies False is False
 -/
 def n2e : Nat → Empty
 | n => _
+
+/-
+Think of the proposition BobStudiesAtUVA as a type and its values as proofs
+-/
+inductive BobStudiesAtUVA
+| attendsClasses
+| paidTuition
+
+inductive MaryStudiesAtUVA
+| attendsClasses
+| paidTuition
+
+inductive MarkoStudiesAtUVA
+
+def neg (α : Type) := α → Empty
+
+example : neg MarkoStudiesAtUVA :=
+λ m : MarkoStudiesAtUVA => nomatch m
+
+/-
+We basically encoded an and of operations to prove that Bob and Mary study at UVA
+-/
+
+inductive BobStudiesAtUVAAndMaryStudiesAtUVA
+| and (b : BobStudiesAtUVA) (m : MaryStudiesAtUVA)
+
+def b : BobStudiesAtUVA := BobStudiesAtUVA.paidTuition
+def m : MaryStudiesAtUVA := MaryStudiesAtUVA.paidTuition
+example : BobStudiesAtUVAAndMaryStudiesAtUVA :=
+  BobStudiesAtUVAAndMaryStudiesAtUVA.and b m
+
+inductive BobStudiesAtUVAOrMaryStudiesAtUVA
+| left (b : BobStudiesAtUVA)
+| right (m : MaryStudiesAtUVA)
+
+example : BobStudiesAtUVAOrMaryStudiesAtUVA :=
+  BobStudiesAtUVAOrMaryStudiesAtUVA.left b
